ab9f7ae9111e71dababe15dc181997399799bb97
ShishaQQ <30622082+ShishaQQ@users.noreply.github.com>
2018-03-23 09:08:50 +0100

    ændringerxd

--- Kebabvognen/Kebabvognen/Pages/Staff.cshtml.cs
+++ Kebabvognen/Kebabvognen/Pages/Staff.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Kebabvognen.Pages
 {
-    public class IndexModel : PageModel
+    public class StaffModel : PageModel
     {
         public void OnGet()
         {
