00579d98ee0ef43839b211ad2450658682a9c31a
Isak Joshua Østergaard Andersen (zbcisand3) <zbcisand3@zbc.dk>
2018-04-03 10:23:09 +0200

    Started on CS

---
+++ Kebabvognen/Kebabvognen/DalAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kebabvognen
+{
+    public abstract class DalAdapter
+    {
+        public abstract bool IsRunning();
+        public abstract void Start();
+        public abstract void Stop();
+        public abstract Menu[] GetMenus();
+
+
+        
+    }
+}
---
+++ Kebabvognen/Kebabvognen/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kebabvognen
+{
+    public class Ingredient
+    {
+        public string Name { get; private set; }
+
+        public Ingredient (string name)
+        {
+            Name = name;
+        }
+    }
+}
