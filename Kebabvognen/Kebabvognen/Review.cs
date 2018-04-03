17b9b871ea40296c3d5668fb62e31c4829d2dbe9
Isak Joshua Østergaard Andersen (zbcisand3) <zbcisand3@zbc.dk>
2018-04-03 12:37:22 +0200

    Alrighty

--- Kebabvognen/Kebabvognen/Review.cs
+++ Kebabvognen/Kebabvognen/Review.cs
@@ -7,18 +7,21 @@ namespace Kebabvognen
 {
     public class Review
     {
+        private int id;
         private string name;
         private byte rating;
         private string description;
         private DateTime date;
 
+        public int Id { get => id; }
         public string Name { get => name; }
         public byte Rating { get => rating; }
         public string Description { get => description; }
         public DateTime Date { get => date; }
 
-        public Review(string name, byte rating, string description, DateTime date)
+        public Review(int id, string name, byte rating, string description, DateTime date)
         {
+            this.id = id;
             this.name = name;
             this.rating = rating;
             this.description = description;
