e586c2504943810a5286abc20b5b60f176a91ffa
Isak Joshua Østergaard Andersen (zbcisand3) <zbcisand3@zbc.dk>
2018-04-03 10:26:51 +0200

    Menu!

---
+++ Kebabvognen/Kebabvognen/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kebabvognen
+{
+    public class Menu
+    {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+        public List<Ingredient> Ingredients { get; private set; }
+
+        public Menu(string name, int price, Ingredient[] ingredients)
+        {
+            Name = name;
+            Price = price;
+            for (int i = 0; i < ingredients.Length; i++)
+                Ingredients.Add(ingredients[i]);
+        }
+
+    }
+}
