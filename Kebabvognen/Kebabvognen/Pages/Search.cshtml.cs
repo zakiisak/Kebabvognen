f892fd7687ad794b97f386b63e8f82ea7bf4f777
Isak Joshua Østergaard Andersen (zbcisand3) <zbcisand3@zbc.dk>
2018-04-05 15:30:51 +0200

    roighty

---
+++ Kebabvognen/Kebabvognen/Pages/Search.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data;
+using Microsoft.AspNetCore.Session;
+using System.Web;
+
+namespace Kebabvognen.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+            System.Diagnostics.Debug.WriteLine("ON GET!");
+            
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("[PAGE-LOAD] Sender: " + sender);
+        }
+    }
+}
